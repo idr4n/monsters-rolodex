6d784825aceb3dbb3eb2ca2a9a873de4f4407a2f
Ivan Duran <idurandiaz@gmail.com>
2020-01-05 13:22:52 +0500

    dynamic content and using map() loop through an array

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Yusuf'
+      monsters: [
+        { name: 'Frankenstein', id: 'asr01' },
+        { name: 'Dracula', id: 'asc23' },
+        { name: 'Zombie', id: 'art15' }
+      ]
     };
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello Ivan' })}>
-            Change Text
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map(monster => {
+          return <h1 key={monster.id}>{monster.name}</h1>;
+        })}
       </div>
     );
   }
