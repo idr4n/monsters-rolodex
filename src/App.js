31c77839c08f1549cf0501750f44b7bc5417e912
Ivan Duran <idurandiaz@gmail.com>
2020-01-08 22:59:54 +0500

    Add filtered monsters

--- src/App.js
+++ src/App.js
@@ -20,12 +20,11 @@ class App extends Component {
   }
 
   render() {
-    const filteredMonsters = this.state.monsters.filter(monster => {
-      // return monster.name.toLowerCase().match(
-      return monster.name.toLowerCase().includes(
-        this.state.searchField.toLowerCase()
-      );
-    });
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => (
+      // return monster.name.toLowerCase().match
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    ));
     return (
       <div className="App">
         <input
