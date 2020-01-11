b430528a3cf471346a9929add99abf1ce11954c3
Ivan Duran <idurandiaz@gmail.com>
2020-01-11 22:54:07 +0500

    Add gradient background and header font

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1 className="header">Monsters Rolodex</h1>
         <SearchBox
           placeHolder="search monsters"
           handleChange={this.handleChange}
